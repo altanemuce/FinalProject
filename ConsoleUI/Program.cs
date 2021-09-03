803da7357b918d82f5c6b53e78c8ad89109b8293
altan emuce <altanemuce61@gmail.com>
2021-09-04 00:51:40 +0300

    9.Hafta

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,14 +13,35 @@ namespace ConsoleUI
 
         static void Main(string[] args)
         {
+            //ProductTest();
+            //CategoryTest();
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.ProductName + " / " + item.CategoryName);
+            }
+
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetByUnitPrice(5,100))
+            foreach (var item in productManager.GetByUnitPrice(5, 100))
             {
                 Console.WriteLine(item.ProductName);
             }
-
-            
         }
     }
 }

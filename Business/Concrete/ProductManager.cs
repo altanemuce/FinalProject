a5d8e59122bde84c1023ed299ef4bfff3cfea657
altan emuce <altanemuce61@gmail.com>
2021-09-09 15:18:43 +0300

    14.Hafta

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
@@ -28,6 +29,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,12 @@ namespace Business.Constans
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExcedeed = "Kategori Limiti Aşıldı";
+        public static string  AuthorizationDenied="Yetkiniz yok";
+        public static  string UserRegistered="Kayıt olundu";
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Parola Hatası";
+        public static string SuccessfulLogin="Başarılı Giriş";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
